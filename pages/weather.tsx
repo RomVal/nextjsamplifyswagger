300b3800f82331de94a10789095e2fcc38b2bf48
Volodymyr <vpyshchyda@findmypast.com>
2025-06-22 21:23:20 +0300

    Add apiUrl variable to use environment variable for API endpoint in Weather component

--- pages/weather.tsx
+++ pages/weather.tsx
@@ -4,6 +4,7 @@ export default function Weather() {
   const [data, setData] = useState<any>(null);
 
   useEffect(() => {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/weather";
     fetch("http://localhost:3001/api/weather") // заміни на URL EC2 після деплою
       .then(res => res.json())
       .then(setData)
