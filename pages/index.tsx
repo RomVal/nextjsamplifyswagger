77893cac820f89e33f5fcf7aab5460e5f5a6a831
Volodymyr <vpyshchyda@findmypast.com>
2025-06-22 21:24:08 +0300

    Update homepage greeting to welcome the Reforge Team

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Hello World 🌍</h1>
+      <h1>Hello Reforge Team!</h1>
       <p>This is the static homepage</p>
     </main>
   );
